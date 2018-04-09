b5d8e56a007b56f76e91bd4a8dd5093c082e4a2d
tahorvat <attila.horvath@gmx.ch>
2018-04-09 13:14:33 +0200

    minor changes

--- hdr/cv_raw2rgb.py
+++ hdr/cv_raw2rgb.py
@@ -2,15 +2,10 @@
 from __future__ import division
 import cv2
 import numpy as np
+from os.path import isfile, join
 np.set_printoptions(threshold=np.nan)
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
-global images_in
-images_in = r'./raw_data/20180325_151016'  # @home
-image_out = r'./output'  # @home
-#images_in = r'C:\Users\tahorvat\PycharmProjects\python_scripts\hdr\input\20171025_140139'  # @lab
-#image_out = r'C:\Users\tahorvat\PycharmProjects\python_scripts\hdr\output'  #@ home  # @ lab
 
 ######################################################################
 ## Hoa: 09.11.2017 Version 1 : raw2rgb.py
@@ -27,35 +22,52 @@ image_out = r'./output'  # @home
 #
 ######################################################################
 
-def main():
-    try:
-        global images_in
-        global image_out
+global Path_to_raw
+Path_to_raw = r'C:\Users\tahorvat\Desktop\20180409_090414'
 
-        images_in = images_in + '/data5_.data'
+def data2rgb(path_to_raw):
+
+    try:
 
         imrows = 2464
         imcols = 3296
 
         imsize = imrows*imcols
 
-        with open(images_in, "rb") as rawimage:
+        with open(path_to_raw, "rb") as rawimage:
             img = np.fromfile(rawimage, np.dtype('u2'), imsize).reshape((imrows, imcols))
 
 
         db_rgb = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2BGR)
         db_rgb = (db_rgb - np.min(db_rgb)) / (np.max(db_rgb) - np.min(db_rgb)) # normalize image
 
-        show_img = True
+        return db_rgb
+
+    except Exception as e:
+        print('Error in data2rgb: ' + str(e))
+
+
+def main():
+    try:
+        global Path_to_raw
+        raw_img_path = Path_to_raw + '/data5_.data'
+
+        show_img = False
+        save_img = True
+
+        db_rgb = data2rgb(raw_img_path)
 
         if(show_img):
             plt.imshow(db_rgb, interpolation='nearest', cmap=cm.binary)
             plt.title('RGB')
             plt.show()
-        else:
+        if(save_img):
             raw_img_name = 'cv_data.rgb'
-            with open(image_out + "/" + raw_img_name, 'wb') as g:
+            print('rgb file path: '+ str(Path_to_raw + '\\' + raw_img_name))
+            with open(join(Path_to_raw,raw_img_name), 'wb') as g:
                 db_rgb.tofile(g)
+
+        print('Done')
 
     except Exception as e:
         print('Error in Main: ' + str(e))
