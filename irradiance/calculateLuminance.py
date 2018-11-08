3ce9d715a4e7df187d996d5718a17ade417facda
ahorv <attila_horvath@gmx.ch>
2018-11-08 14:39:43 +0100

    renamed lumi to irradiance

--- irradiance/calculateLuminance.py
+++ irradiance/calculateLuminance.py
@@ -101,19 +101,6 @@ def LuminanceSquareCrop(LDR_path, sun_x, sun_y, crop_dim):
         f1 = open(image_path1, 'rb')
         im1 = cv2.imread(image_path1)
 
-        # Rotate to correct positions if required
-        lx, ly, dummy = im1.shape
-        if lx>ly:
-            #print ('Rotating')
-            im1 = ndimage.rotate(im1, -90)
-
-        #tags = exifread.process_file(f1)
-        #date_time = tags["EXIF DateTimeDigitized"].values
-        #im_date = date_time[0:10]
-        #im_time = date_time[11:20]
-        #exp_time = tags["EXIF ExposureTime"].values
-        #exp_time1 = exp_time[0].num / exp_time[0].den
-
         centroid_x = sun_x
         centroid_y = sun_y
 
