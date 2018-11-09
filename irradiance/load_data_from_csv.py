37c9300f27e45456dac964ba76531083f5f5ec15
Attila Horvath <Attila.Horvath@ihomelab.ch>
2018-11-09 12:39:02 +0100

    minor changes

--- irradiance/load_data_from_csv.py
+++ irradiance/load_data_from_csv.py
@@ -1,9 +1,6 @@
 import pandas as pd
+import pytz
 
-import numpy as np
-import csv
-import datetime
-from datetime import timedelta
 
 ######################################################################
 ## Hoa: 08.11.2018 Version 1 : load_data_from_csv.py
@@ -30,19 +27,27 @@ from datetime import timedelta
 path_luz = r'irradiation_luz_2017_2018.csv'
 path_soda = r'irradiation_soda_2017_2018.csv'
 
+utc = pytz.UTC
+cest = pytz.timezone('Europe/Zurich')
+
 
 def process_LUZ(csv_file):
     df = pd.read_csv(csv_file, sep=';',index_col = False, header=0)
-    df['time'] = pd.to_datetime(df['time'], format='%Y%m%d%H%M')
+    df['time'] = pd.to_datetime(df['time'], format='%Y%m%d%H%M', utc=True)
+    df['time'] = df['time'].dt.tz_localize('UTC')
+    df['time'] = df['time'].dt.tz_convert('Europe/Zurich')  # converted to central europe time respecting daylight saving
 
     return df
 
 def process_SODA(csv_file):
     df = pd.read_csv(csv_file, sep=';',index_col = False, header=36)
-    # muss den zweiten nehmen
     df['Observation period'] = df['Observation period'].map(lambda x: (((x.split('/')[1]).replace('T',' ')).replace('.0','')))
-    df['Observation period'] = pd.to_datetime(df['Observation period'], format='%Y-%m-%d %H:%M:%S')
+    df['Observation period'] = pd.to_datetime(df['Observation period'], format='%Y-%m-%d %H:%M:%S', utc=True)
     df.rename(columns={'Observation period': 'time'}, inplace=True)
+    df['time'] = df['time'].dt.tz_localize('UTC')
+    df['time'] = df['time'].dt.tz_convert('Europe/Zurich')
+
+    df['time'].to_csv('SODA_out')
 
     return df
 
@@ -56,7 +61,6 @@ def main():
 
     except Exception as e:
        print('MAIN: {}'.format(e))
-
 
 if __name__ == '__main__':
     main()
