06c8ea99fd231a5afb40b2288de73a7d7c39b8d8
ahorv <attila_horvath@gmx.ch>
2018-11-18 17:44:10 +0100

    added luminance from image

--- irradiance/load_data_from_csv.py
+++ irradiance/load_data_from_csv.py
@@ -25,14 +25,14 @@ import pandas as pd
 
 path_luz = r'irradiation_luz_2017_2018.csv'
 path_soda = r'irradiation_soda_2017_2018_1min.csv'
-#path_soda = r'irradiation_soda_2017_2018.csv'
+path_calc = r'20181012_luminance.csv'
 
 
 def process_LUZ(csv_file):
     df = pd.read_csv(csv_file, sep=';',index_col = False, header=0)
     df.set_index(pd.DatetimeIndex(df['time']))
     df['time'] = pd.to_datetime(df['time'], format='%Y%m%d%H%M', utc=True)
-    df['time'] = df['time'].dt.tz_localize('UTC')
+    #df['time'] = df['time'].dt.tz_localize('UTC')
     df['time'] = df['time'].dt.tz_convert('Europe/Zurich')  # converted to central europe time respecting daylight saving
     df.rename(columns={'time': 'datetime'}, inplace=True)
     df['gre000z0'] = pd.to_numeric(df['gre000z0'], errors='coerce')
@@ -45,19 +45,33 @@ def process_SODA(csv_file):
     df.rename(columns={'Observation period': 'datetime'}, inplace=True)
     df['datetime'] = df['datetime'].map(lambda x: (((x.split('/')[1]).replace('T',' ')).replace('.0','')))
     df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H:%M:%S', utc=True)
-    df.set_index(pd.DatetimeIndex(df['datetime']).tz_localize('UTC').tz_convert('Europe/Zurich'), inplace=True)
+    df.set_index(pd.DatetimeIndex(df['datetime']).tz_convert('Europe/Zurich'), inplace=True)
     df = df.loc[df.index.minute % 10 == 0]
     del df['datetime']
 
     #df.to_csv('sodaout_10min.csv')
     return df
 
+def process_CALC(csv_file):
+    df = pd.read_csv(csv_file, sep=',',index_col = False, header=9)
+    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+    df.set_index(pd.DatetimeIndex(df['datetime']).tz_localize('UTC').tz_convert('Europe/Zurich'), inplace=True)
+
+    del df['date']
+    del df['time']
+    del df['no']
+    del df['datetime']
+
+    #df.to_csv('irrad_calc.csv')
+    return df
+
 
 '''
 def main():
     try:
-        df = process_LUZ(path_luz)
-        #df = process_SODA(path_soda)   
+        #df = process_LUZ(path_luz)
+        #df = process_SODA(path_soda)
+        df = process_CALC(path_calc)
         print(df.head(n=10))
 
 
