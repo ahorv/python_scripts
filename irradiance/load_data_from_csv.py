c710aab988e22c375b8ccb6a532f346767a8c363
ahorv <attila_horvath@gmx.ch>
2018-11-19 12:08:36 +0100

    fixed localization in plot

--- irradiance/load_data_from_csv.py
+++ irradiance/load_data_from_csv.py
@@ -5,7 +5,7 @@ import pandas as pd
 ## Hoa: 08.11.2018 Version 1 : load_data_from_csv.py
 ######################################################################
 # process_LUZ()
-# Reads a *.csv file containing irradiation data from a weatherstation
+# Reads a *.csv file containing irradiation data from a weather station
 # near Lucern 47°02'11.0"N 8°18'04.0"E at 454 m.
 #
 # Data contained:
@@ -13,8 +13,16 @@ import pandas as pd
 #  - time: date
 #  - gor000za: Global irradiation standart deviation in W/m²
 #  - gre000z0: mean global irradiation of 10 minutes
-#####################################################################
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # process_SODA()
+# Reads a *.csv file containing irradiation data from :
+# http://www.soda-pro.com/web-services/radiation/cams-mcclear
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# process_CALC()
+# Reads a *.csv file containing mean luminance from HDR image.
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
 # New /Changes:
 # ----------------------------------------------------------------------
@@ -32,7 +40,6 @@ def process_LUZ(csv_file):
     df = pd.read_csv(csv_file, sep=';',index_col = False, header=0)
     df.set_index(pd.DatetimeIndex(df['time']))
     df['time'] = pd.to_datetime(df['time'], format='%Y%m%d%H%M', utc=True)
-    #df['time'] = df['time'].dt.tz_localize('UTC')
     df['time'] = df['time'].dt.tz_convert('Europe/Zurich')  # converted to central europe time respecting daylight saving
     df.rename(columns={'time': 'datetime'}, inplace=True)
     df['gre000z0'] = pd.to_numeric(df['gre000z0'], errors='coerce')
@@ -45,7 +52,8 @@ def process_SODA(csv_file):
     df.rename(columns={'Observation period': 'datetime'}, inplace=True)
     df['datetime'] = df['datetime'].map(lambda x: (((x.split('/')[1]).replace('T',' ')).replace('.0','')))
     df['datetime'] = pd.to_datetime(df['datetime'], format='%Y-%m-%d %H:%M:%S', utc=True)
-    df.set_index(pd.DatetimeIndex(df['datetime']).tz_convert('Europe/Zurich'), inplace=True)
+    df['datetime'] = df['datetime'].dt.tz_convert('Europe/Zurich')
+    df.set_index(pd.DatetimeIndex(df['datetime']), inplace=True)
     df = df.loc[df.index.minute % 10 == 0]
     del df['datetime']
 
@@ -54,25 +62,38 @@ def process_SODA(csv_file):
 
 def process_CALC(csv_file):
     df = pd.read_csv(csv_file, sep=',',index_col = False, header=9)
-    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
-    df.set_index(pd.DatetimeIndex(df['datetime']).tz_localize('UTC').tz_convert('Europe/Zurich'), inplace=True)
+    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], utc=False)
+    df['datetime'] = df['datetime'].dt.tz_localize(tz='Europe/Zurich')
+    #df['datetime'] = df['datetime'].dt.tz_convert(tz='UTC')
+    #df.set_index(pd.DatetimeIndex(df['datetime']).tz_localize('UTC').tz_convert('Europe/Zurich'), inplace=True)
+    df.set_index(df['datetime'],inplace=True)
 
     del df['date']
     del df['time']
     del df['no']
     del df['datetime']
 
-    #df.to_csv('irrad_calc.csv')
+    df.to_csv('irrad_calc.csv')
     return df
-
 
 '''
 def main():
     try:
-        #df = process_LUZ(path_luz)
+        df = process_LUZ(path_luz)
         #df = process_SODA(path_soda)
-        df = process_CALC(path_calc)
-        print(df.head(n=10))
+        #df = process_CALC(path_calc)
+
+        day = '2018-10-12'  # day of observation ('2018-10-12' : camera 2, sw-vers. 3)
+        s_time = ' 07:40:00'  # beginning of observation
+        e_time = ' 07:50:00'  # end of observation
+        start = day + s_time
+        end = day + e_time
+
+        df.set_index(df.datetime, inplace=True)
+        df_lu = df['gre000z0']
+        print(df_lu.loc[start:end])
+
+        #print(df.head(n=10))
 
 
     except Exception as e:
