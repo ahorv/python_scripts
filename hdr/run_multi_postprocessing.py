61cc529fcf2aa220653d8503732f29e4749444a4
ahorv <attila_horvath@gmx.ch>
2018-11-21 18:21:40 +0100

    multiprocessing master file ok

--- hdr/run_multi_postprocessing.py
+++ hdr/run_multi_postprocessing.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import multiprocessing
 from multiprocessing import Pool
 import os
+import time
 
 ######################################################################
 ## Hoa: 19.11.2018 Version: run_multi_processing.py
@@ -12,6 +13,9 @@ import os
 # This is a master file for postprocessing.py.
 # The script starts multiple threads according to the number of
 # CPU cores. Multiple instances of postprocessing.py are run parallel.
+# ----------------------------------------------------------------------
+# Remarks: You must set the python's interpreters path to your individual
+#          configuration !
 #
 # New /Changes:
 # ----------------------------------------------------------------------
@@ -21,9 +25,13 @@ import os
 #
 ######################################################################
 
-def run_process(process):
+global interpreter_path
+
+interpreter_path = r'C:\Users\ati\Anaconda3\envs\skycam\python.exe'
+
+def run_process():
     try:
-        os.system('python {}'.format(process))
+        os.system('{} postprocess.py'.format(interpreter_path))
     except Exception as e:
         print('Error in run_process: {}'.format(e))
 
@@ -32,13 +40,12 @@ def main():
         n_cpus = multiprocessing.cpu_count()
         print('Number of cpu cores: {}'.format(n_cpus))
 
-        processes = ()
+        for i in range(n_cpus - 1):
+            time.sleep(0.3)
+            proc = multiprocessing.Process(target=run_process)
+            proc.start()
 
-        for i in range(n_cpus-1):
-            processes += 'postprocess.py',
-
-        pool = Pool(processes = n_cpus-1)
-        pool.map(run_process, processes)
+        print('Done multiprocessing.')
 
     except Exception as e:
         print('Error in run_multi_postprocessing: {}'.format(e))
